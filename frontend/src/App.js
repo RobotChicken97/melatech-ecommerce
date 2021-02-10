90c7419f48f1bf7baacdd72cd10569d9f69b27d5
RobotChicken97 <unwanafabian@gmail.com>
2021-02-10 19:07:01 +0000

    Products

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,16 @@
+import React from "react";
 import { Container } from "react-bootstrap";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import HomePage from "./pages/HomePage";
 
 const App = () => {
   return (
     <>
       <Header />
-      <main className={"py-3"}>
+      <main className="py-3">
         <Container>
-          <h1>Welcome to MelaTech E-Commmerce</h1>
+          <HomePage />
         </Container>
       </main>
       <Footer />
