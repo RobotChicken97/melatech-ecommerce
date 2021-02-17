be570a050b8b7e031d807c4c0cae0f12a9ae1f9e
RobotChicken97 <unwanafabian@gmail.com>
2021-02-17 21:54:08 +0000

    Checkout steps

--- frontend/src/pages/CheckoutPage.js
+++ frontend/src/pages/CheckoutPage.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Form, Button, Row, Col } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import FormContainer from "../components/FormContainer";
+import CheckoutSteps from "../components/CheckoutSteps";
 import { saveDeliveryAddress } from "../actions/cartActions";
 
 const CheckoutPage = ({ history }) => {
@@ -21,6 +22,7 @@ const CheckoutPage = ({ history }) => {
   };
   return (
     <FormContainer>
+      <CheckoutSteps step1 step2 />
       <h1>Delivery Address</h1>
       <Form onSubmit={submitHandler}>
         <Form.Group controlId={"address"}>
