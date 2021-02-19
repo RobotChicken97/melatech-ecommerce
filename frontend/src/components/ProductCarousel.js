540582dea0465a3bbfe991b12880e6739c937cba
RobotChicken97 <unwanafabian@gmail.com>
2021-02-19 09:13:03 +0000

    Update

--- frontend/src/components/ProductCarousel.js
+++ frontend/src/components/ProductCarousel.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
-import { Carousel, Image } from "react-bootstrap";
+import { Carousel, Col, Image } from "react-bootstrap";
 import Loader from "./Loader";
 import Message from "./Message";
 import { listTopProducts } from "../actions/productActions";
@@ -26,10 +26,11 @@ const ProductCarousel = () => {
         <Carousel.Item key={product._id}>
           <Link to={`/product/${product._id}`}>
             <Image src={product.image} alt={product.name} fluid />
+
             <Carousel.Caption className={"carousel-caption"}>
-              <h2>
+              <h5>
                 {product.name} (₦{product.price})
-              </h2>
+              </h5>
             </Carousel.Caption>
           </Link>
         </Carousel.Item>
